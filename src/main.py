c7c1fe3dffc6ed9f75577315ccefe94ecc22a5f8
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-12-05 15:47:46 +0530

    Added setup for GaussianNB model.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
 
 from constants import Constants
-from utils import convert_to_df, get_rows_as_dicts
+from utils import process_data, get_rows_as_dicts
 
 sc = SparkContext(appName="MLSS")
 spark = SparkSession.builder.getOrCreate()
@@ -11,7 +11,7 @@ ssc = StreamingContext(sc, batchDuration=Constants.BATCH_DURATION)
 lines = ssc.socketTextStream(hostname=Constants.LOCALHOST, port=Constants.PORT)
 
 lines.flatMap(get_rows_as_dicts) \
-    .foreachRDD(lambda rdd: convert_to_df(spark, rdd))
+    .foreachRDD(lambda rdd: process_data(spark, rdd))
 
 ssc.start()
 ssc.awaitTermination()
--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,32 @@
 import json
 
+import numpy as np
 from pyspark import RDD
 from pyspark.ml.feature import StringIndexer
 from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import to_timestamp, hour, month, year
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 from constants import Constants
 
+gaussian_nb_model_global = GaussianNB()
 
-def convert_to_df(spark, rdd):
+
+def convert_data_to_df(spark, data):
     """
     :param spark: SparkSession needed to create dataframe
     :type spark: SparkSession
-    :param rdd:
-    :type rdd: RDD
+    :param data:
+    :type data: dict
+    :return: Input data as dictionary converted to DataFrame
+    :rtype: DataFrame
     """
-    if rdd.isEmpty():
-        return
+    df = spark.createDataFrame(data=data, schema=Constants.DATA_SCHEME) \
+        .toDF(*Constants.COLUMNS)
 
-    df = spark.createDataFrame(data=rdd.collect(), schema=Constants.DATA_SCHEME)
-    df = df.toDF(*Constants.COLUMNS)
     encoding_pairs = {
         Constants.KEY_CATEGORY: "Category_encoded",
         Constants.KEY_DAY_OF_WEEK: "DayOfWeek_encoded",
@@ -30,15 +36,76 @@ def convert_to_df(spark, rdd):
     for input_name, output_name in encoding_pairs.items():
         df = label_encoder(df, input_name, output_name)
 
-    df = df.drop(*list(encoding_pairs.keys()))
-
-    df = df.withColumn("Timestamp", to_timestamp(df["Dates"]))
+    df = df.drop(*list(encoding_pairs.keys())) \
+        .withColumn("Timestamp", to_timestamp(df["Dates"]))
 
     df = df.withColumn("Hour", hour(df["Timestamp"])) \
         .withColumn("Month", month(df["Timestamp"])) \
         .withColumn("Year", year(df["Timestamp"]))
 
     df.show()
+    return df
+
+
+def train_model(df):
+    """
+    :param df:
+    :type df: DataFrame
+    """
+    x_columns = [
+        Constants.KEY_X,
+        Constants.KEY_Y,
+        f"{Constants.KEY_DAY_OF_WEEK}_encoded",
+        f"{Constants.KEY_PD_DISTRICT}_encoded",
+        "Hour",
+        "Month",
+        "Year",
+    ]
+    coord_x = np.asarray(df.select(x_columns).collect())
+    coord_y = np.asarray(df.select(f"{Constants.KEY_CATEGORY}_encoded").collect())
+
+    print(coord_x)
+    print(coord_y)
+
+    types = np.unique(coord_y)
+    print("LENGTH: ", len(types))
+
+    training_x, testing_x, training_y, testing_y = train_test_split(coord_x, coord_y, test_size=0.2, random_state=0)
+
+    # naive bayes classifier used here
+    gaussian_nb_model_local = GaussianNB()
+    gaussian_nb_model_global.partial_fit(training_x, training_y, classes=types)
+    gaussian_nb_model_local.partial_fit(training_x, training_y, classes=types)
+
+    # testing starts here
+    predicted_y_global = gaussian_nb_model_global.predict(testing_x)
+    predicted_y_local = gaussian_nb_model_local.predict(testing_x)
+
+    # Metrics is calculated here
+    metric = np.unique(testing_y)
+    print(metric)
+
+    print("Accuracy of the (Global) model: ", accuracy_score(predicted_y_global, testing_y))
+    print("Accuracy of the (Global) model: ", accuracy_score(predicted_y_global, testing_y))
+    print("Accuracy of the (Local) model: ", accuracy_score(predicted_y_local, testing_y))
+    print("Accuracy of the (Local) model: ", accuracy_score(predicted_y_local, testing_y))
+    print("Classification report:")
+    print(classification_report(y_true=testing_y, y_pred=predicted_y_global, labels=metric))
+    print(classification_report(y_true=testing_y, y_pred=predicted_y_local, labels=metric))
+
+
+def process_data(spark, rdd):
+    """
+    :param spark: SparkSession needed to create dataframe
+    :type spark: SparkSession
+    :param rdd:
+    :type rdd: RDD
+    """
+    if rdd.isEmpty():
+        return
+
+    df = convert_data_to_df(spark, rdd.collect())
+    train_model(df)
 
 
 def get_rows_as_dicts(line):
