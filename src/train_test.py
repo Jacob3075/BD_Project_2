8346bb6b1e556976301ebbed26f42547be85c37c
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-12-06 10:49:49 +0530

    Added Passive Aggressive Classifier.

--- src/train_test.py
+++ src/train_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pyspark import RDD
-from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import SGDClassifier, PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.utils import column_or_1d
@@ -9,6 +9,8 @@ from constants import Constants
 from pre_processing import convert_data_to_df
 
 sgd_classifier = SGDClassifier()
+passive_aggressive_classifier = PassiveAggressiveClassifier()
+
 category_classes = [float(x) for x in list(range(Constants.CATEGORY_LEN))]
 
 
@@ -39,24 +41,25 @@ def train_models(rdd):
     training_x, testing_x, training_y, testing_y = train_test_split(coord_x, coord_y, test_size=0.2, random_state=0)
 
     _train_test_sgd(training_x, training_y, testing_x, testing_y)
+    _train_test_passive_aggressive(training_x, training_y, testing_x, testing_y)
 
 
 def _train_test_sgd(training_x, training_y, testing_x, testing_y):
-    # classifier used here
     sgd_classifier.partial_fit(training_x, training_y, classes=category_classes)
-
-    # testing starts here
     predicted_y_global = sgd_classifier.predict(testing_x)
-
-    # Metrics is calculated here
     _metric_calculation(predicted_y_global, testing_y, model_name="SGD")
+
+
+def _train_test_passive_aggressive(training_x, training_y, testing_x, testing_y):
+    passive_aggressive_classifier.partial_fit(training_x, training_y, classes=category_classes)
+    predicted_y_global = passive_aggressive_classifier.predict(testing_x)
+    _metric_calculation(predicted_y_global, testing_y, model_name="Passive Aggressive")
 
 
 def _metric_calculation(predicted_y, testing_y, model_name):
     print("----------------------------------------------------------------------------------------------")
     print(model_name)
-    metric = np.unique(testing_y)
-    print(metric)
     print("Accuracy of the model: ", accuracy_score(predicted_y, testing_y))
     print("Classification report:")
+    metric = np.unique(testing_y)
     print(classification_report(y_true=testing_y, y_pred=predicted_y, labels=metric))
